48aa03d3a292d31a5ec58f71a35ec49c3401ac85
Thang Le <lethang255@gmail.com>
2019-09-28 11:34:48 +0700

    Effect name updated

--- GPImageEditor/Classes/EffectPage/EffectPageViewModel.swift
+++ GPImageEditor/Classes/EffectPage/EffectPageViewModel.swift
@@ -35,15 +35,15 @@ public class EffectPageViewModel: NSObject {
     public var items: [GPImageFilter] = [
         GPImageFilter(name: "Ảnh gốc", applier: nil),
         GPImageFilter(name: "Giá lạnh", applier: GPImageFilter.clarendonFilter),
-        GPImageFilter(name: "Trầm lắng", applier: GPImageFilter.nashvilleFilter),
-        GPImageFilter(name: "Sôi động", applier: GPImageFilter.toasterFilter),
-        GPImageFilter(name: "Chrome", coreImageFilterName: "CIPhotoEffectChrome"),
-        GPImageFilter(name: "Instant", coreImageFilterName: "CIPhotoEffectInstant"),
-        GPImageFilter(name: "Mono", coreImageFilterName: "CIPhotoEffectMono"),
-        GPImageFilter(name: "Tone", coreImageFilterName: "CILinearToSRGBToneCurve"),
-        GPImageFilter(name: "Noir", coreImageFilterName: "CIPhotoEffectNoir"),
-        GPImageFilter(name: "Process", coreImageFilterName: "CIPhotoEffectProcess"),
-        GPImageFilter(name: "Transfer", coreImageFilterName: "CIPhotoEffectTransfer"),
+        GPImageFilter(name: "Trầm lắng", coreImageFilterName: "CIPhotoEffectProcess"),
+//        GPImageFilter(name: "Trầm lắng", applier: GPImageFilter.nashvilleFilter),
+//        GPImageFilter(name: "Sôi động", applier: GPImageFilter.toasterFilter),
+        GPImageFilter(name: "Sôi động", coreImageFilterName: "CIPhotoEffectTransfer"),
+        GPImageFilter(name: "Hà Nội", coreImageFilterName: "CIPhotoEffectChrome"),
+        GPImageFilter(name: "Huế", coreImageFilterName: "CIPhotoEffectInstant"),
+        GPImageFilter(name: "Hội An", coreImageFilterName: "CIPhotoEffectMono"),
+        GPImageFilter(name: "Sài Gòn", coreImageFilterName: "CILinearToSRGBToneCurve"),
+//        GPImageFilter(name: "Noir", coreImageFilterName: "CIPhotoEffectNoir"),
     ]
     
 }
