815270e876582ccb4b6d8a2ee3656349d0cf3c31
ToanDK <dkt204@gmail.com>
2020-03-13 08:45:54 +0700

    public plugin

--- GPImageEditor/Classes/API/MoyaCacheablePlugin.swift
+++ GPImageEditor/Classes/API/MoyaCacheablePlugin.swift
@@ -9,12 +9,12 @@
 import Foundation
 import Moya
 
-protocol MoyaCacheable {
+public protocol MoyaCacheable {
     typealias MoyaCacheablePolicy = URLRequest.CachePolicy
     var cachePolicy: MoyaCacheablePolicy { get }
 }
 
-final class MoyaCacheablePlugin: PluginType {
+public final class MoyaCacheablePlugin: PluginType {
     func prepare(_ request: URLRequest, target: TargetType) -> URLRequest {
         if let moyaCachableProtocol = target as? MoyaCacheable {
             var cachableRequest = request
